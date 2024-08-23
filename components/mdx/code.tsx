24c65ace7ab7b36244d397cdae14ec85b650ee53
Jesus Santander <jsantanders@outlook.com>
2024-08-23 01:12:45 -0400

    fixes toc

--- components/mdx/code.tsx
+++ components/mdx/code.tsx
@@ -1,4 +1,5 @@
 import { JetBrains_Mono } from "next/font/google";
+import { forwardRef } from "react";
 
 const jetbrains = JetBrains_Mono({
 	weight: "400",
@@ -9,18 +10,18 @@ type CodeProps = {
 	className: string;
 };
 
-export const Code: React.FC<CodeProps> = ({
-	className,
-	...props
-}: CodeProps) => {
-	return (
-		<code
-			className={
-				className
-					? `${className} max-h-3/4 block px-2 py-6 md:px-4 md:py-8`
-					: `${jetbrains.className} rounded bg-muted border border-muted-foreground px-1.5 py-1 text-sm`
-			}
-			{...props}
-		/>
-	);
-};
+export const Code = forwardRef<HTMLElement, CodeProps>(
+	({ className, ...props }, ref) => {
+		return (
+			<code
+				ref={ref}
+				className={
+					className
+						? `${className} max-h-3/4 block px-2 py-6 md:px-4 md:py-8`
+						: `${jetbrains.className} rounded bg-muted border border-muted-foreground px-1.5 py-1 text-sm`
+				}
+				{...props}
+			/>
+		);
+	},
+);
--- components/post/post-table-of-content.tsx
+++ components/post/post-table-of-content.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { useHighlighted } from "@/hooks/use-highlighted";
-import { generateId } from "@/lib/generate-id";
 import { Link } from "@/navigation";
 import type { Toc, TocEntry } from "@stefanprobst/rehype-extract-toc";
+import { useCallback, useEffect, useState } from "react";
 
 type PostTableOfContentProps = {
 	toc: Toc;
@@ -20,55 +19,126 @@ export const PostTableOfContents: React.FC<PostTableOfContentProps> = ({
 	toc,
 	locales,
 }) => {
+	const [activeSection, setActiveSection] = useState(toc[0]?.id);
+	const concatTocIds = useCallback((toc: Toc) => {
+		const result: string[] = [];
+
+		function traverse(entry: Toc[0]) {
+			if (entry.id) {
+				result.push(entry.id);
+			}
+
+			if (entry.children) {
+				entry.children.forEach(traverse);
+			}
+		}
+
+		toc.forEach(traverse);
+
+		return result;
+	}, []);
+
+	useEffect(() => {
+		const observers = concatTocIds(toc).map((id) => {
+			const element = document.getElementById(id);
+			if (!element) return null;
+
+			const observer = new IntersectionObserver(
+				([entry]) => {
+					if (entry?.isIntersecting) {
+						setActiveSection(id);
+					}
+				},
+				{
+					rootMargin: "44px 0% -35% 0px",
+				},
+			);
+
+			observer.observe(element);
+			return observer;
+		});
+
+		return () => {
+			// biome-ignore lint/complexity/noForEach: <explanation>
+			observers.forEach((observer) => observer?.disconnect());
+		};
+	}, [toc, concatTocIds]);
+
 	if (!toc?.length) {
 		return null;
 	}
 
 	return (
 		<nav>
-			<h5 className="md:translate-y-h5 mb-4 translate-y-1 text-base font-bold uppercase">
+			<h5 className="md:translate-y-h5 mb-4 translate-y-1 font-bold uppercase">
 				{locales.title}
 			</h5>
-			{renderNodes(toc)}
+			<ListOfContent
+				nodes={toc}
+				activeSection={activeSection}
+				setActiveSectionId={setActiveSection}
+			/>
 		</nav>
 	);
 };
 
-function renderNodes(nodes: Toc, chapter = "") {
+const ListOfContent: React.FC<{
+	nodes: Toc;
+	chapter?: string;
+	activeSection?: string;
+	setActiveSectionId: React.Dispatch<React.SetStateAction<string | undefined>>;
+}> = ({ nodes, activeSection, setActiveSectionId, chapter = "" }) => {
 	return (
 		<ul>
 			{nodes
 				.filter((n) => !NON_TOC_ELEMENTS.includes(n.value))
 				.map((node, idx) => (
 					<li key={node.id}>
-						<TOCLink node={node} ch={`${chapter}${idx + 1}.`} />
-						{node.children?.length &&
-							node.children?.length > 0 &&
-							renderNodes(node.children, `${chapter}${idx + 1}.`)}
+						<TOCLink
+							node={node}
+							ch={`${chapter}${idx + 1}.`}
+							isHighlighted={activeSection === node.id}
+							onNavigate={setActiveSectionId}
+						/>
+						{node.children?.length && node.children?.length > 0 && (
+							<ListOfContent
+								activeSection={activeSection}
+								setActiveSectionId={setActiveSectionId}
+								nodes={node.children}
+								chapter={`${chapter}${idx + 1}.`}
+							/>
+						)}
 					</li>
 				))}
 		</ul>
 	);
-}
+};
 
-const TOCLink: React.FC<{ node: TocEntry; ch: string }> = ({ node, ch }) => {
+const TOCLink: React.FC<{
+	node: TocEntry;
+	ch: string;
+	isHighlighted: boolean;
+	onNavigate: (entry: string) => void;
+}> = ({ node, ch, isHighlighted, onNavigate }) => {
 	const fontSizes: Record<number, string> = { 2: "sm", 3: "sm", 4: "xs" };
 	const padding: Record<number, string> = { 2: "pl-0", 3: "pl-7", 4: "pl-10" };
-	const id = node.id || generateId(6);
-	const [highlighted, setHighlighted] = useHighlighted(id);
+	const id = node.id;
+
+	if (!id) return null;
+
 	return (
 		<Link
 			href={`#${id}`}
 			className={`block text-${fontSizes[node.depth]} ${
 				padding[node.depth]
 			} border-l py-1 pl-3 hover:border-primary hover:text-primary ${
-				highlighted
+				isHighlighted
 					? "border-primary text-primary"
 					: "border-muted text-muted-foreground"
 			}`}
 			onClick={(e) => {
 				e.preventDefault();
-				setHighlighted(id);
+				onNavigate(id);
 				document
 					.getElementById(id)
 					?.scrollIntoView({ behavior: "smooth", block: "start" });
