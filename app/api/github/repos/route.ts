5bc3bf55a140afbbf870d4d006c278c898d217bc
Jesus Santander <jsantanders@outlook.com>
2024-10-31 12:58:29 -0400

    fixes cache

--- app/api/github/repos/route.ts
+++ app/api/github/repos/route.ts
@@ -29,6 +29,9 @@ export const GET = async (): Promise<Response> => {
 			Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
 		},
 		body: JSON.stringify({ query }),
+		next: {
+			revalidate: 60,
+		},
 	}).then((r) => r.json());
 
 	return Response.json({ repos: response.data });
---
+++ content/posts/2024-10-28--getting-env-from-remote-nextjs-docker/index.md
@@ -0,0 +1,9 @@
+---
+title: Linear Regression in R, exploring a real-world dataset
+summary: Using this dataset we will explore linear regression concepts and techniques.
+isPublished: true
+publishedAt: "2023-04-24"
+tags:
+  - linear-regression
+  - oceanography
+---
